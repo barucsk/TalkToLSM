888d7847d4d969b404ffa62d0c0bdf6f81760da4
fredoobolo <fredoobolo@gmail.com>
2020-03-24 22:48:02 -0600

    Interfaz Grafica

--- Assets/Scripts/MainButtonScript.cs
+++ Assets/Scripts/MainButtonScript.cs
@@ -5,14 +5,17 @@ using UnityEngine.UI;
 
 public class MainButtonScript : MonoBehaviour
 {
-    public Animator anim;
+    public Animator animOptionButton;
     public Animator animSoundButton;
-    
+    public Animator animTraductorButton;
+    public GameObject soundButton;
+    public GameObject traductorButton;
+
 
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
@@ -24,23 +27,27 @@ public class MainButtonScript : MonoBehaviour
 
     public void showOptions()
     {
-        GameObject button = GameObject.Find("SoundButton");
-        anim = GetComponent<Animator>();
-        bool value = anim.GetBool("DESPLEGAR");
-        animSoundButton = button.GetComponent<Animator>();
+        animOptionButton = GetComponent<Animator>(); 
+        soundButton = GameObject.Find("SoundButton");
+        traductorButton = GameObject.Find("TraductorButton");
+        animSoundButton = soundButton.GetComponent<Animator>();
+        animTraductorButton = traductorButton.GetComponent<Animator>();
+
         bool valueSB = animSoundButton.GetBool("DESPLEGAR");
+        bool valueOB = animOptionButton.GetBool("DESPLEGAR");
+        bool valueTB = animTraductorButton.GetBool("DESPLEGAR");
 
 
-        if (value == true) 
+        if (valueOB == true) 
         {
-            anim.SetBool("DESPLEGAR", false);
-
+            animOptionButton.SetBool("DESPLEGAR", false);
+            animTraductorButton.SetBool("DESPLEGAR", false);
             animSoundButton.SetBool("DESPLEGAR", false);
         }
         else
         {
-            anim.SetBool("DESPLEGAR", true);
-
+            animOptionButton.SetBool("DESPLEGAR", true);
+            animTraductorButton.SetBool("DESPLEGAR", true);
             animSoundButton.SetBool("DESPLEGAR", true);
         }
     }
